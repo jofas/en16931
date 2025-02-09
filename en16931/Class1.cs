408cb85a3be4ca0283a9c2117482933452fcb59b
jofas <jonas@fc-web.de>
2025-02-09 18:06:57 +0100

    inital project setup

---
+++ en16931/Class1.cs
@@ -0,0 +1,8 @@
+﻿namespace dev.fassbender.en16931;
+
+public class Class1
+{
+    public static bool test() {
+        return true;
+    }
+}
---
+++ tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿using dev.fassbender.en16931;
+
+namespace tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(Class1.test());
+    }
+}
