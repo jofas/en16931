811bf6226eef8156e19fcc6cad5af1316ff89ddf
Jonas Fassbender <jonas@fassbender.dev>
2025-03-26 17:07:50 +0100

    started implementing xrechnung validation

--- en16931/src/Validator.cs
+++ en16931/src/Validator.cs
@@ -1,9 +1,129 @@
 ﻿namespace dev.fassbender.en16931;
 
+using System;
+using System.Xml;
+using System.Xml.Schema;
+
 public class Validator
 {
     public static void ValidateFromFile(string filepath)
     {
-        throw new System.NotImplementedException();
+        XmlDocument doc = new XmlDocument();
+        doc.Load(filepath);
+        XmlNode root = doc.DocumentElement;
+
+        XmlNamespaceManager ublNamespaceManager = new XmlNamespaceManager(doc.NameTable);
+
+        ublNamespaceManager.AddNamespace("ubl", "urn:oasis:names:specification:ubl:schema:xsd:Invoice-2");
+        ublNamespaceManager.AddNamespace("cac", "urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2");
+        ublNamespaceManager.AddNamespace("cbc", "urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2");
+        ublNamespaceManager.AddNamespace("invoice", "urn:oasis:names:specification:ubl:schema:xsd:Invoice-2");
+        ublNamespaceManager.AddNamespace("creditnote", "urn:oasis:names:specification:ubl:schema:xsd:CreditNote-2");
+
+        XmlNamespaceManager ciiNamespaceManager = new XmlNamespaceManager(doc.NameTable);
+
+        ciiNamespaceManager.AddNamespace("rsm", "urn:un:unece:uncefact:data:standard:CrossIndustryInvoice:100");
+        ciiNamespaceManager.AddNamespace("ram", "urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100");
+        ciiNamespaceManager.AddNamespace("qdt", "urn:un:unece:uncefact:data:standard:QualifiedDataType:100");
+        ciiNamespaceManager.AddNamespace("udt", "urn:un:unece:uncefact:data:standard:UnqualifiedDataType:100");
+
+        // UBL Invoice CIUS
+        XmlNode? identifier = root.SelectSingleNode(
+            "/invoice:Invoice/cbc:CustomizationID[ . = 'urn:cen.eu:en16931:2017#compliant#urn:xeinkauf.de:kosit:xrechnung_3.0']",
+            ublNamespaceManager
+        );
+        Schema? schema = Schema.UblInvoice;
+
+        // UBL Invoice XRechnung Extension
+        if (identifier == null) {
+            identifier = root.SelectSingleNode(
+                "/invoice:Invoice/cbc:CustomizationID[ . = 'urn:cen.eu:en16931:2017#compliant#urn:xeinkauf.de:kosit:xrechnung_3.0#conformant#urn:xeinkauf.de:kosit:extension:xrechnung_3.0']",
+                ublNamespaceManager
+            );
+         }
+
+        // UBL CreditNote XRechnung CIUS
+        if (identifier == null) {
+            identifier = root.SelectSingleNode(
+                "/creditnote:CreditNote[ cbc:CustomizationID/text() = 'urn:cen.eu:en16931:2017#compliant#urn:xeinkauf.de:kosit:xrechnung_3.0']",
+                ublNamespaceManager
+            );
+            schema = Schema.UblCreditNote;
+        }
+
+        // CII XRechnung CIUS
+        if (identifier == null) {
+            identifier = root.SelectSingleNode(
+                "/rsm:CrossIndustryInvoice[rsm:ExchangedDocumentContext/ram:GuidelineSpecifiedDocumentContextParameter/ram:ID/text() = 'urn:cen.eu:en16931:2017#compliant#urn:xeinkauf.de:kosit:xrechnung_3.0']",
+                ciiNamespaceManager
+            );
+            schema = Schema.CiiCrossIndustryInvoice;
+        }
+
+        // CII XRechnung Extension
+        if (identifier == null) {
+            identifier = root.SelectSingleNode(
+                "/rsm:CrossIndustryInvoice[rsm:ExchangedDocumentContext/ram:GuidelineSpecifiedDocumentContextParameter/ram:ID/text() = 'urn:cen.eu:en16931:2017#compliant#urn:xeinkauf.de:kosit:xrechnung_3.0#conformant#urn:xeinkauf.de:kosit:extension:xrechnung_3.0']",
+                ciiNamespaceManager
+            );
+            schema = Schema.CiiCrossIndustryInvoice;
+        }
+
+        if (identifier == null) {
+            throw new Exception("unable to find identifier");
+        }
+
+        schema = schema!;
+
+        string schemaFilePath = schema switch {
+            Schema.UblInvoice => "resources/ubl/2.1/maindoc/UBL-Invoice-2.1.xsd",
+            Schema.UblCreditNote => "resources/ubl/2.1/maindoc/UBL-CreditNote-2.1.xsd",
+            Schema.CiiCrossIndustryInvoice => "resources/cii/d16b/CrossIndustryInvoice_100pD16B.xsd",
+        };
+
+        doc.Schemas.Add(null, schemaFilePath);
+
+        // TODO: fix schema compilation (probably relative imports)
+        //   https://learn.microsoft.com/en-us/dotnet/fundamentals/runtime-libraries/system-xml-schema-xmlschemaset#security-considerations
+
+        /*
+        doc.Schemas.XmlResolver = new XmlResolver();
+        //XmlSchemaValidationFlags.ProcessSchemaLocation;
+
+        foreach (XmlSchema s in doc.Schemas.Schemas()) {
+            foreach (var include in s.Includes) {
+                Console.WriteLine((include as XmlSchemaImport).SchemaLocation);
+            }
+        }
+        */
+
+        doc.Schemas.Compile();
+
+        ValidationEventHandler eventHandler = new ValidationEventHandler(ValidationEventHandler);
+        doc.Validate(eventHandler);
+
+        Console.WriteLine("validated schema");
+
+        // TODO: Saxon-HE: validate against en16931 schematron
+        // TODO: Saxon-HE: validate against xrechnung schematron
     }
+
+    static void ValidationEventHandler(object sender, ValidationEventArgs e)
+    {
+        switch (e.Severity)
+        {
+            case XmlSeverityType.Error:
+                Console.WriteLine("Error: {0}", e.Message);
+                break;
+            case XmlSeverityType.Warning:
+                Console.WriteLine("Warning {0}", e.Message);
+                break;
+        }
+    }
+}
+
+enum Schema {
+    UblInvoice,
+    UblCreditNote,
+    CiiCrossIndustryInvoice,
 }
--- tests/src/XRechnungValidation.cs
+++ tests/src/XRechnungValidation.cs
@@ -4,6 +4,16 @@ namespace tests;
 
 public class XRechnungValidation
 {
+    [Fact]
+    public void Test()
+    {
+        string pathUbl = "resources/xrechnung-testsuite/standard/01.01a-INVOICE_ubl.xml";
+        Validator.ValidateFromFile(pathUbl);
+
+        string pathCii = "resources/xrechnung-testsuite/standard/01.01a-INVOICE_uncefact.xml";
+        Validator.ValidateFromFile(pathCii);
+    }
+    /*
     [Theory]
     [InlineData("resources/xrechnung-testsuite/standard")]
     [InlineData("resources/xrechnung-testsuite/extension")]
@@ -17,4 +27,5 @@ public class XRechnungValidation
             Validator.ValidateFromFile(standardTest);
         }
     }
+    */
 }
