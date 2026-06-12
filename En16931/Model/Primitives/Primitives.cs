cd7930394f703bf5323f26d1cd8a103d42f61018
Jonas Fassbender <jonas@fassbender.dev>
2026-06-12 11:03:48 +0200

    IIRSerializable for primitives

--- En16931/Model/Primitives/Primitives.cs
+++ En16931/Model/Primitives/Primitives.cs
@@ -9,47 +9,72 @@ using En16931.Utils;
 
 namespace En16931.Model.Primitives;
 
-public readonly record struct Amount(decimal Value) : IIRDeserializable<Amount>
+public readonly record struct Amount(decimal Value) : IIRDeserializable<Amount>, IIRSerializable
 {
+    public void Serialize(XmlWriter writer)
+    {
+        writer.WriteValue(Value);
+    }
+
     public static Amount Deserialize(XmlReader reader)
     {
         return new Amount(reader.ReadContentAsDecimal());
     }
 }
 
-public readonly record struct Percentage(decimal Value) : IIRDeserializable<Percentage>
+public readonly record struct Percentage(decimal Value) : IIRDeserializable<Percentage>, IIRSerializable
 {
+    public void Serialize(XmlWriter writer)
+    {
+        writer.WriteValue(Value);
+    }
+
     public static Percentage Deserialize(XmlReader reader)
     {
         return new Percentage(reader.ReadContentAsDecimal());
     }
 }
 
-public readonly record struct Quantity(decimal Value) : IIRDeserializable<Quantity>
+public readonly record struct Quantity(decimal Value) : IIRDeserializable<Quantity>, IIRSerializable
 {
+    public void Serialize(XmlWriter writer)
+    {
+        writer.WriteValue(Value);
+    }
+
     public static Quantity Deserialize(XmlReader reader)
     {
         return new Quantity(reader.ReadContentAsDecimal());
     }
 }
 
-public readonly record struct UnitPriceAmount(decimal Value) : IIRDeserializable<UnitPriceAmount>
+public readonly record struct UnitPriceAmount(decimal Value) : IIRDeserializable<UnitPriceAmount>, IIRSerializable
 {
+    public void Serialize(XmlWriter writer)
+    {
+        writer.WriteValue(Value);
+    }
+
     public static UnitPriceAmount Deserialize(XmlReader reader)
     {
         return new UnitPriceAmount(reader.ReadContentAsDecimal());
     }
 }
 
-public readonly record struct Date(DateTime Value) : IIRDeserializable<Date>
+public readonly record struct Date(DateTime Value) : IIRDeserializable<Date>, IIRSerializable
 {
+    public void Serialize(XmlWriter writer)
+    {
+        writer.WriteString($"{Value.Year:D4}-{Value.Month:D2}-{Value.Day:D2}");
+    }
+
     public static Date Deserialize(XmlReader reader)
     {
         return new Date(reader.ReadContentAsDateTime());
     }
 }
 
-public readonly record struct Code : IIRDeserializable<Code>
+public readonly record struct Code : IIRDeserializable<Code>, IIRSerializable
 {
     public required string Value
     {
@@ -71,13 +96,18 @@ public readonly record struct Code : IIRDeserializable<Code>
         Value = value;
     }
 
+    public void Serialize(XmlWriter writer)
+    {
+        writer.WriteString(Value);
+    }
+
     public static Code Deserialize(XmlReader reader)
     {
         return new Code(reader.ReadContentAsString());
     }
 }
 
-public readonly record struct DocumentReference : IIRDeserializable<DocumentReference>
+public readonly record struct DocumentReference : IIRDeserializable<DocumentReference>, IIRSerializable
 {
     public required string Value
     {
@@ -99,13 +129,18 @@ public readonly record struct DocumentReference : IIRDeserializable<DocumentRefe
         Value = value;
     }
 
+    public void Serialize(XmlWriter writer)
+    {
+        writer.WriteString(Value);
+    }
+
     public static DocumentReference Deserialize(XmlReader reader)
     {
         return new DocumentReference(reader.ReadContentAsString());
     }
 }
 
-public readonly record struct Text : IIRDeserializable<Text>
+public readonly record struct Text : IIRDeserializable<Text>, IIRSerializable
 {
     public required string Value
     {
@@ -125,6 +160,11 @@ public readonly record struct Text : IIRDeserializable<Text>
     public Text(string value)
     {
         Value = value;
+    }
+
+    public void Serialize(XmlWriter writer)
+    {
+        writer.WriteString(Value);
     }
 
     public static Text Deserialize(XmlReader reader)
@@ -232,7 +272,7 @@ public readonly record struct BinaryObject : IIRDeserializable<BinaryObject>, II
     }
 }
 
-public readonly record struct Identifier : IIRDeserializable<Identifier>
+public readonly record struct Identifier : IIRDeserializable<Identifier>, IIRSerializable
 {
     public required string Content
     {
@@ -274,6 +314,27 @@ public readonly record struct Identifier : IIRDeserializable<Identifier>
         Content = content;
         SchemeIdentifier = schemeIdentifier;
         SchemeVersionIdentifier = schemeVersionIdentifier;
+    }
+
+    public void Serialize(XmlWriter writer)
+    {
+        writer.WriteStartElement("content", IRConfig.NS);
+        writer.WriteString(Content);
+        writer.WriteEndElement();
+
+        if (SchemeIdentifier is not null)
+        {
+            writer.WriteStartElement("scheme-identifier", IRConfig.NS);
+            writer.WriteString(SchemeIdentifier);
+            writer.WriteEndElement();
+        }
+
+        if (SchemeVersionIdentifier is not null)
+        {
+            writer.WriteStartElement("scheme-version-identifier", IRConfig.NS);
+            writer.WriteString(SchemeVersionIdentifier);
+            writer.WriteEndElement();
+        }
     }
 
     public static Identifier Deserialize(XmlReader reader)
--- Tests/IR/Serialize.cs
+++ Tests/IR/Serialize.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Xml;
 using En16931.Model;
@@ -8,6 +9,39 @@ namespace Tests.IR;
 
 public class Serialize
 {
+    [Fact]
+    public void SerializeDate()
+    {
+        string expected = """
+            <date xmlns="urn:todo">2018-04-13</date>
+            """;
+
+        Date value = new Date(new DateTime(2018, 4, 13));
+
+        using StringWriter writer = new();
+        using XmlTextWriter xmlWriter = new(writer)
+        {
+            Formatting = Formatting.Indented,
+        };
+
+        xmlWriter.WriteStartElement("date", "urn:todo");
+        value.Serialize(xmlWriter);
+        xmlWriter.WriteEndElement();
+
+        string actual = writer.ToString();
+
+        Assert.Equal(expected, actual);
+
+        using StringReader reader = new(actual);
+        using XmlTextReader xmlReader = new(reader);
+
+        xmlReader.ReadStartElement("date");
+        xmlReader.MoveToContent();
+
+        var roundTrip = Date.Deserialize(xmlReader);
+
+        Assert.Equal(value, roundTrip);
+    }
 
     [Fact]
     public void SerializeBinaryObject()
